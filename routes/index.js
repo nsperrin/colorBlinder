3b6a7e1ed81fa420425a2465860e9b52aa9761a6
nsperrin <nsperrin@mckendree.edu>
2016-09-08 21:25:16 -0500

    changed title on page to be my temp app name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ColorBlinder' });
 });
 
 module.exports = router;
