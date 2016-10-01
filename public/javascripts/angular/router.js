2c78c085c878796b52ec2c47fd29599c347eedf6
nsperrin <nsperrin@mckendree.edu>
2016-10-01 16:15:40 -0500

    added gulp and set up my tests as gulp tasks

--- public/javascripts/angular/router.js
+++ public/javascripts/angular/router.js
@@ -3,6 +3,77 @@
  */
 
 colorBlinder.config(function($stateProvider, $urlRouterProvider) {
+    var uHome = {
+        url:'/home',
+        views:{
+            header: {
+                templateUrl: 'partials/header/unverified.html'
+            },
+            subHeader:{
+                template:""
+            },
+            popUp:{
+                template:""
+            },
+            main:{
+                templateUrl:"partials/main/home.html"
+            }
+        }
+    };
+
+    var uHomeLogin = {
+        url: '/home',
+        views: {
+            header: {
+                templateUrl: 'partials/header/unverified.html'
+            },
+            subHeader: {
+                template: ""
+            },
+            popUp: {
+                templateUrl: "partials/popUp/login.html"
+            },
+            main: {
+                templateUrl: "partials/main/home.html"
+            }
+        }
+    };
+
+    var uHomeSignUp = {
+        url:'/home',
+        views:{
+            header: {
+                templateUrl: 'partials/header/unverified.html'
+            },
+            subHeader:{
+                template:""
+            },
+            popUp:{
+                templateUrl:"partials/popUp/signup.html"
+            },
+            main:{
+                templateUrl:"partials/main/home.html"
+            }
+        }
+    };
+
+    var uCreate = {
+        url:'/create',
+        views:{
+            header: {
+                templateUrl: 'partials/header/unverified.html'
+            },
+            subHeader:{
+                template:""
+            },
+            popUp:{
+                template:""
+            },
+            main:{
+                templateUrl:"partials/main/create.html"
+            }
+        }
+    };
 
     $urlRouterProvider.otherwise('/home');
 
@@ -11,77 +82,4 @@ colorBlinder.config(function($stateProvider, $urlRouterProvider) {
         .state('uHomeLogin',uHomeLogin)
         .state('uHomeSignUp',uHomeSignUp)
         .state('uCreate',uCreate)
-
 });
-
-var uHome = {
-    url:'/home',
-    views:{
-        header: {
-            templateUrl: 'partials/header/unverified.html'
-        },
-        subHeader:{
-            template:""
-        },
-        popUp:{
-            template:""
-        },
-        main:{
-            templateUrl:"partials/main/home.html"
-        }
-    }
-};
-
-var uHomeLogin = {
-    url: '/home',
-    views: {
-        header: {
-            templateUrl: 'partials/header/unverified.html'
-        },
-        subHeader: {
-            template: ""
-        },
-        popUp: {
-            templateUrl: "partials/popUp/login.html"
-        },
-        main: {
-            templateUrl: "partials/main/home.html"
-        }
-    }
-};
-
-var uHomeSignUp = {
-    url:'/home',
-    views:{
-        header: {
-            templateUrl: 'partials/header/unverified.html'
-        },
-        subHeader:{
-            template:""
-        },
-        popUp:{
-            templateUrl:"partials/popUp/signup.html"
-        },
-        main:{
-            templateUrl:"partials/main/home.html"
-        }
-    }
-};
-
-var uCreate = {
-    url:'/create',
-    views:{
-        header: {
-            templateUrl: 'partials/header/unverified.html'
-        },
-        subHeader:{
-            template:""
-        },
-        popUp:{
-            template:""
-        },
-        main:{
-            templateUrl:"partials/main/create.html"
-        }
-    }
-};
