84b38e23bcb4b00b6d0092a1426126cc05fb96c4
nsperrin <nsperrin@mckendree.edu>
2016-10-02 21:23:57 -0500

    added some test stubs. I need to actually right the tests

---
+++ tests/protractor/loginTests.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Nick on 10/2/2016.
+ */
+describe('Tests for the login popup', function(){
+
+    beforeEach(function(done){
+        //sign out if I am logged in
+
+        //navigate to the login popup
+        done();
+    });
+
+    afterEach(function(done){
+        //sign out if I am logged in
+        done();
+    });
+
+    it('has a place for email', function(done){
+        done();
+    });
+
+    it('has a place for password', function(done){
+        done();
+    });
+
+    it('has a place for remembering me', function(done){
+        done();
+    });
+
+    it('has a place for the submit button', function(done){
+        done();
+    });
+
+    it('the email only accepts *@*', function(done){
+        done();
+    });
+
+    it('if I submit bad credentials then I stay on the login popup and have a warning message', function(done){
+        done();
+    });
+
+    it('if I sumbit good credentials then I am directed to the verified version of that page', function(done){
+        done();
+    });
+});
---
+++ tests/protractor/signUpTests.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Nick on 10/1/2016.
+ */
+describe('Signing Up', function(){
+
+    beforeEach(function(done){
+        //do the signing out if I'm signed in
+
+        //open the sigin popup
+        done();
+    });
+
+    afterEach(function(done){
+        //sign out if I'm signed in
+        done()
+    });
+
+    it('has a place for email',function(done){
+        done();
+    });
+
+    it('has a place for password',function(done){
+        done();
+    });
+
+    it('has a place for password confirmation',function(done){
+        done();
+    });
+
+    it('has a place for remembering me', function(done){
+        done();
+    });
+
+    it('has a place for the submit button', function(done){
+        done();
+    });
+
+    it('the email position only accepts strings in the format *@*',function(done){
+        done();
+    });
+
+    it('the passwords must be the same before submission',function(done){
+        done();
+    });
+
+    it('when I submit I am added to the database',function(done){
+        done();
+    });
+
+    it('when I submit I am directed to the verified version of the page',function(done){
+        done();
+    });
+});
