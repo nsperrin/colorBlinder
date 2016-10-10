be722ea32b65609423f4399a539b7518792d1253
nsperrin <nsperrin@mckendree.edu>
2016-10-09 19:43:29 -0500

    added some tests

--- public/javascripts/angular/master.js
+++ public/javascripts/angular/master.js
@@ -5,6 +5,22 @@ colorBlinder.controller('master', ['$scope', '$state', function($scope,$state) {
     $scope.stateMachine = new stateMachine();
     $scope.userData;
     $scope.currentScheme;
+    $scope.signup = {
+        email:null,
+        password:null,
+        passwordConfirm:null
+    };
+
+    $scope.isSignUpDisabled = function(){
+        return !($scope.signup.email !== null && $scope.signup.email !== undefined && $scope.signup.email !== ""
+            && $scope.signup.password !== null && $scope.signup.password !== undefined && $scope.signup.password !== ""
+            && $scope.signup.passwordConfirm !== null && $scope.signup.passwordConfirm !== undefined && $scope.signup.passwordConfirm !== ""
+            && typeof $scope.signup.email === 'string'
+            && typeof $scope.signup.password === 'string'
+            && typeof $scope.signup.passwordConfirm === 'string'
+            && $scope.signup.password == $scope.signup.passwordConfirm);
+
+    }
 
     $scope.changeState = function(action){
         $scope.stateMachine.getNext($state.$current,action,function(newState){
@@ -18,4 +34,5 @@ colorBlinder.controller('master', ['$scope', '$state', function($scope,$state) {
     $scope.home          = function(){$scope.changeState('home');};
     $scope.myColors      = function(){$scope.changeState('myColors');};
     $scope.logout        = function(){$scope.changeState('logout');};
+
 }]);
--- tests/protractor/signUpTests.js
+++ tests/protractor/signUpTests.js
@@ -7,53 +7,109 @@ describe('Signing Up', function(){
         //do the signing out if I'm signed in
 
         //open the sigin popup
+        browser.get("https://localhost:8443/");
+        element(by.css(".signup")).click();
         done();
     });
 
     afterEach(function(done){
         //sign out if I'm signed in
-
+        element(by.css(".signup")).click();
         //remove any test users
         done()
     });
 
     it('has a place for email',function(done){
+        expect(element(by.css("#signup #inputEmail")).isPresent()).toBeTruthy();
         done();
     });
 
     it('has a place for password',function(done){
+        expect(element(by.css("#signup #inputPassword")).isPresent()).toBeTruthy();
         done();
     });
 
     it('has a place for password confirmation',function(done){
+        expect(element(by.css("#signup #inputConfirmPassword")).isPresent()).toBeTruthy();
         done();
     });
 
     it('has a place for remembering me', function(done){
+        expect(element(by.css("#signup #signUpRememberMe")).isPresent()).toBeTruthy();
         done();
     });
 
     it('has a place for the submit button', function(done){
+        expect(element(by.css("#signup #signUpButton")).isPresent()).toBeTruthy();
         done();
     });
 
     it('the email position only accepts strings in the format *@*',function(done){
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputEmail")).sendKeys("");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputEmail")).clear();
+        element(by.css("#signup #inputEmail")).sendKeys("test");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputEmail")).clear();
+        element(by.css("#signup #inputEmail")).sendKeys("@test");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputEmail")).clear();
+        element(by.css("#signup #inputEmail")).sendKeys("test@test");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputEmail")).clear();
+        element(by.css("#signup #inputEmail")).sendKeys("test@test.test");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeFalsy();
+        element(by.css("#signup #inputEmail")).clear();
         done();
     });
 
     it('the passwords must be the same before submission',function(done){
+        element(by.css("#signup #inputEmail")).sendKeys("test@test.test");
+        element(by.css("#signup #inputPassword")).sendKeys("1");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("2");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputPassword")).clear();
+        element(by.css("#signup #inputConfirmPassword")).clear();
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("2");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputPassword")).clear();
+        element(by.css("#signup #inputConfirmPassword")).clear();
+        element(by.css("#signup #inputPassword")).sendKeys("1");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputPassword")).clear();
+        element(by.css("#signup #inputConfirmPassword")).clear();
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd2");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeTruthy();
+        element(by.css("#signup #inputPassword")).clear();
+        element(by.css("#signup #inputConfirmPassword")).clear();
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd");
+        expect(element(by.css('#signup #signUpButton')).getAttribute('disabled')).toBeFalsy();
+        element(by.css("#signup #inputPassword")).clear();
+        element(by.css("#signup #inputConfirmPassword")).clear();
         done();
     });
 
     it('when I submit I am added to the database',function(done){
+        element(by.css("#signup #inputEmail")).sendKeys("test@test.test");
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd");
+        element(by.css('#signup #signUpButton')).click();
+        browser.sleep(1000);
         done();
     });
 
     it('when I submit I am directed to the verified version of the page',function(done){
-        done();
-    });
-
-    it('when I sign up with a remember me, and leave the page and come back, I am logged in', function(done){
+        element(by.css("#signup #inputEmail")).sendKeys("test@test.test");
+        element(by.css("#signup #inputPassword")).sendKeys("testP@$$W0rd");
+        element(by.css("#signup #inputConfirmPassword")).sendKeys("testP@$$W0rd");
+        element(by.css('#signup #signUpButton')).click();
+        browser.sleep(1000);
         done();
     });
 });
